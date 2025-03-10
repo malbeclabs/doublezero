1595ee6ba92914f82c235fd71c6e37b2bab4916e
Steve Shaw <steve@malbeclabs.com>
2025-03-10 09:54:56 -0400

    tests: add e2e test

---
+++ e2e/e2e_test.go
@@ -0,0 +1,240 @@
+//go:build e2e
+
+package e2e_test
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	nl "github.com/vishvananda/netlink"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/malbeclabs/doublezero/controlplane/proto/controller/gen/pb-go"
+)
+
+var (
+	// Environmental variables here are exported via the calling shell script (e2e_test.sh)
+	controllerAddr = "localhost"
+	controllerPort = os.Getenv("CONTROLLER_PORT")
+	agentPubKey    = os.Getenv("AGENT_PUBKEY")
+
+	// expected doublezero address to be allocated to client during test
+	doublezeroAddr = "64.86.249.81/32"
+	// expected link-local address to be allocated to the client during test
+	linkLocalAddr = "169.254.0.1/31"
+
+	//go:embed fixtures/*
+	fs embed.FS
+)
+
+// TestClientOutput is a set of tests to verify the output of the doublezero
+// CLI. These tests utilize golden files of expected output stored in the fixtures directory,
+// which are then compared against the std output of each command line call.
+//
+// Based on the current behavior of the CLI, the output can change with dynamic data which
+// cause lines to be sorted differently (i.e. different public keys are generated for a device)
+// from test run to test run. Because of this, we treat desired and test generated output as
+// slices of strings and verify each line of the desired output is present in the test generated
+// output slice.
+func TestClientOutput(t *testing.T) {
+	tests := []struct {
+		name       string
+		goldenFile string
+		cmd        []string
+	}{
+		{
+			name:       "doublezero_user_list",
+			goldenFile: "fixtures/doublezero_user_list_user_added.txt",
+			cmd:        []string{"doublezero", "user", "list"},
+		},
+		{
+			name:       "doublezero_device_list",
+			goldenFile: "fixtures/doublezero_device_list.txt",
+			cmd:        []string{"doublezero", "device", "list"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			want, err := fs.ReadFile(test.goldenFile)
+			if err != nil {
+				t.Fatalf("error reading golden file %s: %v", test.goldenFile, err)
+			}
+			got, err := exec.Command(test.cmd[0], test.cmd[1:]...).Output()
+			if err != nil {
+				t.Fatalf("error running cmd %s: %v", test.cmd, err)
+			}
+			opts := []cmp.Option{
+				cmpopts.SortSlices(func(a, b string) bool { return a < b }),
+			}
+			if diff := cmp.Diff(strings.Split(string(want), "\n"), strings.Split(string(got), "\n"), opts...); diff != "" {
+				t.Fatalf("output mismatch: -(want), +(got):%s", diff)
+			}
+		})
+	}
+}
+
+// TestConnect verifies the client and agent configuration after a
+// user smartcontract has been established.
+func TestConnect(t *testing.T) {
+	t.Run("check_tunnel_interface_is_configured", func(t *testing.T) {
+		tun, err := nl.LinkByName("doublezero0")
+		if err != nil {
+			t.Fatalf("error fetching tunnel status: %v", err)
+		}
+		if tun.Attrs().Name != "doublezero0" {
+			t.Fatalf("tunnel name is not doublezero0: %s", tun.Attrs().Name)
+		}
+		if tun.Attrs().OperState != 0 { // 0 == IF_OPER_UNKNOWN
+			t.Fatalf("tunnel is not set to up state (6), got %d", tun.Attrs().OperState)
+		}
+		if tun.Attrs().MTU != 1476 {
+			t.Fatalf("tunnel mtu should be 1476; got %d", tun.Attrs().MTU)
+		}
+		// TODO: check IP address configuration
+		// TODO: check routing tables
+		// TODO: check IP rules
+	})
+	t.Run("check_doublezero_address_is_configured", func(t *testing.T) {
+		tun, err := nl.LinkByName("doublezero0")
+		if err != nil {
+			t.Fatalf("error fetching tunnel status: %v", err)
+		}
+		addrs, err := nl.AddrList(tun, nl.FAMILY_V4)
+		if err != nil {
+			t.Fatalf("error fetching tunnel addresses: %v", err)
+		}
+		// want, err := nl.ParseAddr(doublezeroAddr)
+		// if err != nil {
+		// 	t.Fatalf("error parsing address: %v", err)
+		// }
+		findAddr := func(addr string) (bool, error) {
+			want, err := nl.ParseAddr(addr)
+			if err != nil {
+				return false, fmt.Errorf("error parsing address: %v", err)
+			}
+			return slices.ContainsFunc(addrs, func(a nl.Addr) bool {
+				return a.Equal(*want)
+			}), nil
+		}
+		found, err := findAddr(doublezeroAddr)
+		if err != nil {
+			t.Fatalf("error while checking for doublezero address: %v", err)
+		}
+		if !found {
+			t.Fatalf("doublezero address %s not found on tunnel\n", doublezeroAddr)
+		}
+		found, err = findAddr(linkLocalAddr)
+		if err != nil {
+			t.Fatalf("error while checking for link-local address: %v", err)
+		}
+		if !found {
+			t.Fatalf("link-local address %s not found on tunnel\n", doublezeroAddr)
+		}
+	})
+	t.Run("check_agent_configuration", func(t *testing.T) {
+		target := net.JoinHostPort(controllerAddr, controllerPort)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		opts := []grpc.DialOption{
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		}
+		conn, err := grpc.NewClient(target, opts...)
+		if err != nil {
+			log.Fatalf("error creating controller client: %v", err)
+		}
+		defer conn.Close()
+		defer cancel()
+
+		agent := pb.NewControllerClient(conn)
+		got, err := agent.GetConfig(ctx, &pb.ConfigRequest{Pubkey: agentPubKey})
+		if err != nil {
+			log.Fatalf("error while fetching config: %v\n", err)
+		}
+		want, err := fs.ReadFile("fixtures/doublezero_agent_config_user_added.txt")
+		if err != nil {
+			t.Fatalf("error loading expected agent configuration from file: %v", err)
+		}
+
+		if diff := cmp.Diff(string(want), got.Config); diff != "" {
+			t.Fatalf("output mismatch: -(want), +(got): %s", diff)
+		}
+	})
+}
+
+// TestDisconnect verifies the client and agent configuration after a
+// user has been disconnected.
+func TestDisconnect(t *testing.T) {
+	t.Run("check_tunnel_interface_is_removed", func(t *testing.T) {
+		links, err := nl.LinkList()
+		if err != nil {
+			t.Fatalf("error fetching links: %v\n", err)
+		}
+		found := slices.ContainsFunc(links, func(l nl.Link) bool {
+			return l.Attrs().Name == "doublezero0"
+		})
+		if found {
+			t.Fatal("doublezero0 tunnel interface not removed on disconnect")
+		}
+	})
+
+	t.Run("check_user_contract_is_removed", func(t *testing.T) {
+		goldenFile := "fixtures/doublezero_user_list_user_removed.txt"
+		want, err := fs.ReadFile(goldenFile)
+		if err != nil {
+			t.Fatalf("error reading golden file %s: %v", goldenFile, err)
+		}
+		cmd := []string{"doublezero", "user", "list"}
+		got, err := exec.Command(cmd[0], cmd[1:]...).Output()
+		if err != nil {
+			t.Fatalf("error running cmd %s: %v", cmd, err)
+		}
+		opts := []cmp.Option{
+			cmpopts.SortSlices(func(a, b string) bool { return a < b }),
+		}
+		if diff := cmp.Diff(strings.Split(string(want), "\n"), strings.Split(string(got), "\n"), opts...); diff != "" {
+			t.Fatalf("output mismatch: -(want), +(got):%s", diff)
+		}
+	})
+
+	t.Run("check_user_tunnel_is_removed_from_agent", func(t *testing.T) {
+		target := net.JoinHostPort(controllerAddr, controllerPort)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		opts := []grpc.DialOption{
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		}
+		conn, err := grpc.NewClient(target, opts...)
+		if err != nil {
+			log.Fatalf("error creating controller client: %v", err)
+		}
+		defer conn.Close()
+		defer cancel()
+
+		agent := pb.NewControllerClient(conn)
+		got, err := agent.GetConfig(ctx, &pb.ConfigRequest{Pubkey: agentPubKey})
+		if err != nil {
+			log.Fatalf("error while fetching config: %v\n", err)
+		}
+		want, err := fs.ReadFile("fixtures/doublezero_agent_config_user_removed.txt")
+		if err != nil {
+			t.Fatalf("error loading expected agent configuration from file: %v", err)
+		}
+
+		if diff := cmp.Diff(string(want), got.Config); diff != "" {
+			t.Fatalf("output mismatch: -(want), +(got): %s", diff)
+		}
+	})
+}
---
+++ e2e/start_e2e.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+set -e
+
+SOLANA_KEYPAIR=/root/.config/solana/id.json
+VALIDATOR_URL=http://127.0.0.1:8899
+VALIDATOR_WS=ws://127.0.0.1:8900
+PROGRAM_ID=7CTniUa88iJKUHTrCkB4TjAoG6TD7AMivhQeuqN2LPtX
+
+CONTROLLER_ADDR=0.0.0.0
+CONTROLLER_PORT=7000
+export CONTROLLER_ADDR
+export CONTROLLER_PORT
+
+# This is the pubkey for the agent correlating to ny5-dz01
+# Device pubkeys are created deterministically based on the order
+# in which they are created onchain.
+# WARNING - IF YOU CHANGE THE DEVICE CREATION ORDERING, THIS WILL BE
+# INCORRECT AND THE TEST WILL FAIL
+AGENT_PUBKEY=8scDVeZ8aB1TRTkBqaZgzxuk7WwpARdF1a39wYA7nR3W
+AGENT_DEVICE=ny5-dz01
+export AGENT_PUBKEY
+
+main() {
+    print_banner "Starting local validator w/ smartcontract program"
+    solana-test-validator --reset --bpf-program ./bin/keypair.json ./bin/double_zero_sla_program.so > /tmp/solana.log 2>&1 &
+    echo "Waiting 15 seconds to start the solana test cluster"
+    sleep 15
+
+    print_banner "Initialize doublezero client configuration"
+    init_doublezero
+
+    print_banner "Initialize smart contract"
+    doublezero --keypair $SOLANA_KEYPAIR init
+
+    print_banner "Initializing activator"
+    doublezero-activator --program-id $PROGRAM_ID  > /tmp/activator.log 2>&1 &
+
+    print_banner "Initializing onchain data"
+    populate_data_onchain
+
+    print_banner "Initializing doublezero controller"
+    doublezero-controller start -listen-addr 0.0.0.0 -listen-port 7000 -program-id $PROGRAM_ID -solana-rpc-endpoint $VALIDATOR_URL &
+
+    print_banner "Initializing doublezero daemon"
+    start_doublezerod
+
+    print_banner "Waiting for latency results (75 second timeout)"
+    sleep 75
+
+    print_banner "Latency results"
+    doublezero latency
+
+    print_banner "Connecting user tunnel"
+    doublezero --keypair $SOLANA_KEYPAIR connect --client-ip 1.2.3.4
+
+    print_banner "Wait for controller to pickup new user"
+    sleep 30
+
+    print_banner "Running command line output tests"
+    e2e_test -test.v -test.run "^TestClientOutput$"
+
+    print_banner "Running user connect tests"
+    e2e_test -test.v -test.run "^TestConnect$"
+
+    print_banner "Disconnecting user tunnel"
+    doublezero --keypair $SOLANA_KEYPAIR disconnect --client-ip 1.2.3.4
+    
+    print_banner "Wait for controller to pickup disconnected user"
+    sleep 30
+
+    doublezero user list
+    print_banner "Running user disconnect tests"
+    e2e_test -test.v -test.run "^TestDisconnect$"
+}
+
+print_banner() {
+    echo "------------------------------------------------"
+    echo $*
+    echo "------------------------------------------------"
+}
+
+init_doublezero() {
+    solana config set --url $VALIDATOR_URL
+
+    doublezero --keypair $SOLANA_KEYPAIR config set --url $VALIDATOR_URL
+    doublezero --keypair $SOLANA_KEYPAIR config set --ws $VALIDATOR_WS
+    doublezero --keypair $SOLANA_KEYPAIR config set --program $PROGRAM_ID
+}
+
+start_doublezerod() {
+    # create path for socket file
+    mkdir /var/run/doublezerod
+    # create state file directory
+    mkdir /var/lib/doublezerod
+    doublezerod -program-id $PROGRAM_ID -solana-rpc-endpoint $VALIDATOR_URL &
+}
+
+populate_data_onchain() {
+    print_banner "Populate global configuration onchain"
+    doublezero global-config set --local-asn 65100 --remote-asn 65001 --tunnel-tunnel-block 172.16.0.0/16 --device-tunnel-block 169.254.0.0/16
+    print_banner "Global configuration onchain"
+    doublezero global-config get
+
+    print_banner "Populate location information onchain"
+    doublezero --keypair $SOLANA_KEYPAIR location create --code lax --name "Los Angeles" --country US --lat 34.049641274076464 --lng -118.25939642499903
+    doublezero location create --code ewr --name "New York" --country US --lat 40.780297071772125 --lng -74.07203003496925
+    doublezero location create --code lhr --name "London" --country UK --lat 51.513999803939384 --lng -0.12014764843092213
+    doublezero location create --code fra --name "Frankfurt" --country DE --lat 50.1215356432098 --lng 8.642047117175098
+    doublezero location create --code sin --name "Singapore" --country SG --lat 1.2807150707390342 --lng 103.85507136144396
+    doublezero location create --code tyo --name "Tokyo" --country JP --lat 35.66875144228767 --lng 139.76565267564501
+    doublezero location create --code pit --name "Pittsburgh" --country US --lat 40.45119259881935 --lng -80.00498215509094
+    doublezero location create --code ams --name "Amsterdam" --country US --lat 52.30085793004002 --lng 4.942241140085309
+    print_banner "Location information onchain"
+    doublezero location list
+
+    print_banner "Populate exchange information onchain"
+    doublezero exchange create --code xlax --name "Los Angeles" --lat 34.049641274076464 --lng -118.25939642499903
+    doublezero exchange create --code xewr --name "New York" --lat 40.780297071772125 --lng -74.07203003496925
+    doublezero exchange create --code xlhr --name "London" --lat 51.513999803939384 --lng -0.12014764843092213
+    doublezero exchange create --code xfra --name "Frankfurt" --lat 50.1215356432098 --lng 8.642047117175098
+    doublezero exchange create --code xsin --name "Singapore" --lat 1.2807150707390342 --lng 103.85507136144396
+    doublezero exchange create --code xtyo --name "Tokyo" --lat 35.66875144228767 --lng 139.76565267564501
+    doublezero exchange create --code xpit --name "Pittsburgh" --lat 40.45119259881935 --lng -80.00498215509094
+    doublezero exchange create --code xams --name "Amsterdam" --lat 52.30085793004002 --lng 4.942241140085309
+    print_banner "Exchange information onchain"
+    doublezero exchange list
+
+
+    print_banner "Populate device information onchain - DO NOT SHUFFLE THESE AS THE PUBKEYS WILL CHANGE"
+    doublezero device create --code la2-dz01 --location lax --exchange xlax --public-ip "207.45.216.134" --dz-prefix "207.45.216.136/29"
+    doublezero device create --code ny5-dz01 --location ewr --exchange xewr --public-ip "64.86.249.80" --dz-prefix "64.86.249.80/29"
+    doublezero device create --code ld4-dz01 --location lhr --exchange xlhr --public-ip "195.219.120.72" --dz-prefix "195.219.120.72/29"
+    doublezero device create --code frk-dz01 --location fra --exchange xfra --public-ip "195.219.220.88" --dz-prefix "195.219.220.88/29"
+    doublezero device create --code sg1-dz01 --location sin --exchange xsin --public-ip "180.87.102.104" --dz-prefix "180.87.102.104/29"
+    doublezero device create --code ty2-dz01 --location tyo --exchange xtyo --public-ip "180.87.154.112" --dz-prefix "180.87.154.112/29"
+    doublezero device create --code pit-dzd01 --location pit --exchange xpit --public-ip "204.16.241.243" --dz-prefix "204.16.243.243/32"
+    doublezero device create --code ams-dz001 --location ams --exchange xams --public-ip "195.219.138.50" --dz-prefix "195.219.138.56/29"
+    print_banner "Device information onchain"
+    doublezero device list
+
+    # print_banner "Populate device IPs as loopbacks for latency measurement.\n"
+    print_banner "Adding ny5-dz01 IP as loopback for latency testing; blackhole others."
+    ip addr add 64.86.249.80 dev lo
+    ip route add blackhole 207.45.216.134/32
+    ip route add blackhole 195.219.120.72/32
+    ip route add blackhole 195.219.220.88/32
+    ip route add blackhole 180.87.102.104/32
+    ip route add blackhole 180.87.154.112/32
+    ip route add blackhole 204.16.241.243/32
+    ip route add blackhole 195.219.138.50/32
+
+    ip addr list dev lo
+    ip route list
+
+    print_banner "Populate tunnel information onchain"
+    doublezero tunnel create --code "la2-dz01:ny5-dz01" --side-a la2-dz01 --side-z ny5-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 40 --jitter-ms 3
+    doublezero tunnel create --code "ny5-dz01:ld4-dz01" --side-a ny5-dz01 --side-z ld4-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 30 --jitter-ms 3
+    doublezero tunnel create --code "ld4-dz01:frk-dz01" --side-a ld4-dz01 --side-z frk-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 25 --jitter-ms 10
+    doublezero tunnel create --code "ld4-dz01:sg1-dz01" --side-a ld4-dz01 --side-z sg1-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 120 --jitter-ms 9
+    doublezero tunnel create --code "sg1-dz01:ty2-dz01" --side-a sg1-dz01 --side-z ty2-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 40 --jitter-ms 7
+    doublezero tunnel create --code "ty2-dz01:la2-dz01" --side-a ty2-dz01 --side-z la2-dz01 --tunnel-type MPLSoGRE --bandwidth "10 Gbps" --mtu 9000 --delay-ms 30 --jitter-ms 10
+    print_banner "Tunnel information onchain"
+    doublezero tunnel list
+}
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+main "$@"; exit

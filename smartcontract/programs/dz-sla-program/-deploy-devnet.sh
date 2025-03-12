e632cc81a1694453eca3329c89f36c265d294e1c
Juan Olveira <juan@malbeclabs.com>
2025-03-12 19:26:57 -0300

    Smart contract cleaning (#82)

--- smartcontract/programs/dz-sla-program/-deploy-devnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-solana config set --url devnet
-cargo build-sbf
-
-solana program deploy --program-id ./target/deploy/double_zero_sla_program-keypair.json ./target/deploy/double_zero_sla_program.so
-
-#python3 update.py

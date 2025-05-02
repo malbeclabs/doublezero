df35cc36739e38ce01856b2867bea4dc12eda2bf
Ben <bgm@malbeclabs.com>
2025-05-02 16:08:21 -0700

    !#86 - sort user/device list

--- smartcontract/sdk/rs/src/commands/device/list.rs
+++ smartcontract/sdk/rs/src/commands/device/list.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use crate::DoubleZeroClient;
 use doublezero_sla_program::state::{
     accountdata::AccountData, accounttype::AccountType, device::Device,
@@ -9,14 +7,22 @@ use solana_sdk::pubkey::Pubkey;
 pub struct ListDeviceCommand {}
 
 impl ListDeviceCommand {
-    pub fn execute(&self, client: &dyn DoubleZeroClient) -> eyre::Result<HashMap<Pubkey, Device>> {
-        Ok(client
+    pub fn execute(&self, client: &dyn DoubleZeroClient) -> eyre::Result<Vec<(Pubkey, Device)>> {
+        let mut sorted_device_list: Vec<(Pubkey, Device)> = client
             .gets(AccountType::Device)?
             .into_iter()
             .map(|(k, v)| match v {
                 AccountData::Device(device) => (k, device),
                 _ => panic!("Invalid Account Type"),
             })
-            .collect())
+            .collect();
+
+
+        sorted_device_list.sort_by(|(_, a), (_, b)| {
+              a.code.cmp(&b.code)
+          });
+
+
+        Ok(sorted_device_list)
     }
 }
--- smartcontract/sdk/rs/src/commands/user/list.rs
+++ smartcontract/sdk/rs/src/commands/user/list.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use crate::DoubleZeroClient;
 use doublezero_sla_program::state::{
     accountdata::AccountData, accounttype::AccountType, user::User,
@@ -9,14 +7,23 @@ use solana_sdk::pubkey::Pubkey;
 pub struct ListUserCommand {}
 
 impl ListUserCommand {
-    pub fn execute(&self, client: &dyn DoubleZeroClient) -> eyre::Result<HashMap<Pubkey, User>> {
-        Ok(client
+    pub fn execute(&self, client: &dyn DoubleZeroClient) -> eyre::Result<Vec<(Pubkey, User)>> {
+        let mut sorted_user_list: Vec<(Pubkey, User)> = client
             .gets(AccountType::User)?
             .into_iter()
             .map(|(k, v)| match v {
                 AccountData::User(user) => (k, user),
                 _ => panic!("Invalid Account Type"),
             })
-            .collect())
+            .collect();
+
+        sorted_user_list.sort_by(|(_, a), (_, b)| {
+              a.device_pk
+                  .cmp(&b.device_pk)
+                  .then_with(|| a.tunnel_id.cmp(&b.tunnel_id))
+          });
+
+
+        Ok(sorted_user_list)
     }
 }

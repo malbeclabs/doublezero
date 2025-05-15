6572582f02e6f2c3505de378ca8e77db4e6a4050
Rahul Garg <rg@vihu.dev>
2025-05-15 19:07:37 +0400

    Fix: Cleanup clippy lints smartcontracts
    Summary
    ----
    Very small PR to cleanup and fix clippy lint warnings under the
    smartcontracts workspace.

--- smartcontract/cli/src/doublezerocommand.rs
+++ smartcontract/cli/src/doublezerocommand.rs
@@ -162,7 +162,7 @@ pub struct CliCommandImpl<'a> {
 
 impl CliCommandImpl<'_> {
     pub fn new(client: &DZClient) -> CliCommandImpl {
-        CliCommandImpl { client: client }
+        CliCommandImpl { client }
     }
 }
 

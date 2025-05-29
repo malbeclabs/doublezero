f1e1e4bdc303e142afbc05a6777fa9bfbc12769f
Steven Normore <snormore@gmail.com>
2025-05-29 17:05:40 -0400

    feat(controller): multicast support (#421)
    * SDK: Multicast support
    
    Adds support for new multicast related fields in smartcontract.
    
    * feat(controller): multicast device/tunnels config for agent
    
    * fix(controller): no ip access-list standard SEC-USER-MCAST-BOUNDARY
    
    * chore(controller): unicast tunnel description
    
    * fix(controller): deny 0.0.0.0/0 subscriber-only tunnel
    
    * fix(e2e): update agent device/tunnels config for multicast
    
    * fix(controller): multicast pub/sub acl
    
    ---------
    
    Co-authored-by: Greg Mitchell <greg@malbeclabs.com>

--- controlplane/controller/internal/controller/render_test.go
+++ controlplane/controller/internal/controller/render_test.go
@@ -16,9 +16,10 @@ func TestRenderConfig(t *testing.T) {
 		Want        string
 	}{
 		{
-			Name:        "render_tunnels_successfully",
-			Description: "render config for a set of tunnels",
+			Name:        "render_unicast_tunnels_successfully",
+			Description: "render config for a set of unicast tunnels",
 			Data: templateData{
+				MulticastGroupBlock: "239.0.0.0/24",
 				Device: &Device{
 					PublicIP: net.IP{7, 7, 7, 7},
 					Tunnels: []*Tunnel{
@@ -53,12 +54,13 @@ func TestRenderConfig(t *testing.T) {
 				},
 				UnknownBgpPeers: []net.IP{},
 			},
-			Want: "fixtures/tunnel.txt",
+			Want: "fixtures/unicast.tunnel.txt",
 		},
 		{
 			Name:        "render_peer_removal_successfully",
 			Description: "render config for removal of unknown peers successfully",
 			Data: templateData{
+				MulticastGroupBlock: "239.0.0.0/24",
 				Device: &Device{
 					PublicIP: net.IP{7, 7, 7, 7},
 					Tunnels: []*Tunnel{
@@ -97,6 +99,124 @@ func TestRenderConfig(t *testing.T) {
 			},
 			Want: "fixtures/unknown.peer.removal.txt",
 		},
+		{
+			Name:        "render_multicast_tunnel_successfully",
+			Description: "render config for a multicast tunnel",
+			Data: templateData{
+				MulticastGroupBlock: "239.0.0.0/24",
+				Device: &Device{
+					PublicIP: net.IP{7, 7, 7, 7},
+					Tunnels: []*Tunnel{
+						{
+							Id:            500,
+							UnderlaySrcIP: net.IP{1, 1, 1, 1},
+							UnderlayDstIP: net.IP{2, 2, 2, 2},
+							OverlaySrcIP:  net.IP{169, 254, 0, 0},
+							OverlayDstIP:  net.IP{169, 254, 0, 1},
+							DzIp:          net.IP{100, 0, 0, 0},
+							Allocated:     true,
+							IsMulticast:   true,
+							MulticastSubscribers: []net.IP{
+								{239, 0, 0, 1},
+								{239, 0, 0, 2},
+							},
+							MulticastPublishers: []net.IP{},
+						},
+						{
+							Id:                   501,
+							UnderlaySrcIP:        net.IP{3, 3, 3, 3},
+							UnderlayDstIP:        net.IP{4, 4, 4, 4},
+							OverlaySrcIP:         net.IP{169, 254, 0, 2},
+							OverlayDstIP:         net.IP{169, 254, 0, 3},
+							DzIp:                 net.IP{100, 0, 0, 1},
+							Allocated:            true,
+							IsMulticast:          true,
+							MulticastSubscribers: []net.IP{},
+							MulticastPublishers: []net.IP{
+								{239, 0, 0, 3},
+								{239, 0, 0, 4},
+							},
+						},
+						{
+							Id:            502,
+							UnderlaySrcIP: net.IP{5, 5, 5, 5},
+							UnderlayDstIP: net.IP{6, 6, 6, 6},
+							OverlaySrcIP:  net.IP{169, 254, 0, 4},
+							OverlayDstIP:  net.IP{169, 254, 0, 5},
+							DzIp:          net.IP{100, 0, 0, 2},
+							Allocated:     true,
+							IsMulticast:   true,
+							MulticastSubscribers: []net.IP{
+								{239, 0, 0, 5},
+								{239, 0, 0, 6},
+							},
+							MulticastPublishers: []net.IP{
+								{239, 0, 0, 5},
+								{239, 0, 0, 6},
+							},
+						},
+					},
+				},
+				UnknownBgpPeers: []net.IP{},
+			},
+			Want: "fixtures/multicast.tunnel.txt",
+		},
+		{
+			Name:        "render_mixed_tunnels_successfully",
+			Description: "render config for a mix of unicast and multicast tunnels",
+			Data: templateData{
+				MulticastGroupBlock: "239.0.0.0/24",
+				Device: &Device{
+					PublicIP: net.IP{7, 7, 7, 7},
+					Tunnels: []*Tunnel{
+						{
+							Id:            500,
+							UnderlaySrcIP: net.IP{1, 1, 1, 1},
+							UnderlayDstIP: net.IP{2, 2, 2, 2},
+							OverlaySrcIP:  net.IP{169, 254, 0, 0},
+							OverlayDstIP:  net.IP{169, 254, 0, 1},
+							DzIp:          net.IP{100, 0, 0, 0},
+							Allocated:     true,
+							IsMulticast:   true,
+							MulticastSubscribers: []net.IP{
+								{239, 0, 0, 1},
+								{239, 0, 0, 2},
+							},
+							MulticastPublishers: []net.IP{},
+						},
+						{
+							Id:            501,
+							UnderlaySrcIP: net.IP{3, 3, 3, 3},
+							UnderlayDstIP: net.IP{4, 4, 4, 4},
+							OverlaySrcIP:  net.IP{169, 254, 0, 2},
+							OverlayDstIP:  net.IP{169, 254, 0, 3},
+							DzIp:          net.IP{100, 0, 0, 1},
+							Allocated:     true,
+						},
+						{
+							Id:            502,
+							UnderlaySrcIP: net.IP{5, 5, 5, 5},
+							UnderlayDstIP: net.IP{6, 6, 6, 6},
+							OverlaySrcIP:  net.IP{169, 254, 0, 4},
+							OverlayDstIP:  net.IP{169, 254, 0, 5},
+							DzIp:          net.IP{100, 0, 0, 2},
+							Allocated:     true,
+							IsMulticast:   true,
+							MulticastSubscribers: []net.IP{
+								{239, 0, 0, 3},
+								{239, 0, 0, 4},
+							},
+							MulticastPublishers: []net.IP{
+								{239, 0, 0, 5},
+								{239, 0, 0, 6},
+							},
+						},
+					},
+				},
+				UnknownBgpPeers: []net.IP{},
+			},
+			Want: "fixtures/mixed.tunnel.txt",
+		},
 	}
 
 	for _, test := range tests {
@@ -113,6 +233,5 @@ func TestRenderConfig(t *testing.T) {
 				t.Errorf("renderTunnels mismatch (-want +got): %s\n", diff)
 			}
 		})
-
 	}
 }

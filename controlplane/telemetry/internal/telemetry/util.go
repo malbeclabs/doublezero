7af7985d7a3fdd3cb04bcaf852ba64f8c003f648
Steven Normore <snormore@gmail.com>
2025-06-25 13:16:49 -0400

    agent/telem: seed rng in submitter

--- controlplane/telemetry/internal/telemetry/util.go
+++ controlplane/telemetry/internal/telemetry/util.go
@@ -2,13 +2,8 @@ package telemetry
 
 import (
 	"context"
-	"math/rand"
 	"time"
 )
-
-func randFloat64() float64 {
-	return rand.Float64()
-}
 
 func sleepOrDone(ctx context.Context, d time.Duration) bool {
 	timer := time.NewTimer(d)

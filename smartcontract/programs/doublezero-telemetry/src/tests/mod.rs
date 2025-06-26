fd8330746d83f8f4b07edba468c46d52c82a9006
Steven Normore <snormore@gmail.com>
2025-06-26 12:28:07 -0400

    sc/telem: suppress noisy test logs except on failure

--- smartcontract/programs/doublezero-telemetry/src/tests/mod.rs
+++ smartcontract/programs/doublezero-telemetry/src/tests/mod.rs
@@ -5,3 +5,12 @@ pub mod test_helpers;
 mod initialize_dz_samples_tests;
 #[cfg(test)]
 mod write_dz_samples_tests;
+
+#[cfg(test)]
+#[ctor::ctor]
+fn init_logger() {
+    let _ = env_logger::builder()
+        // Suppress noisy solana program logs unless the test fails.
+        .is_test(true)
+        .try_init();
+}

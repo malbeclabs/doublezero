fb24f54f5ae776d9d7002d2b921f137368d801fa
jeffgrunewald <jeff.grunewald@gmail.com>
2025-07-14 23:02:44 -0400

    add shutdown signal listening for activator

--- smartcontract/cli/src/subscribe.rs
+++ smartcontract/cli/src/subscribe.rs
@@ -1,6 +1,9 @@
 use clap::Args;
 use doublezero_sdk::DZClient;
-use std::io::Write;
+use std::{
+    io::Write,
+    sync::{atomic::AtomicBool, Arc},
+};
 
 #[derive(Args, Debug)]
 pub struct SubscribeCliCommand;
@@ -9,11 +12,14 @@ impl SubscribeCliCommand {
     pub fn execute<W: Write>(self, client: &DZClient, out: &mut W) -> eyre::Result<()> {
         println!("Waiting for events...");
 
-        client.subscribe(|_, pubkey, account| {
-            if let Err(e) = writeln!(out, "{pubkey} -> {account:?}") {
-                eprintln!("Failed to write output: {e}");
-            }
-        })?;
+        client.subscribe(
+            |_, pubkey, account| {
+                if let Err(e) = writeln!(out, "{pubkey} -> {account:?}") {
+                    eprintln!("Failed to write output: {e}");
+                }
+            },
+            Arc::new(AtomicBool::new(false)),
+        )?;
 
         Ok(())
     }

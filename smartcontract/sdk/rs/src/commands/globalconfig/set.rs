7e0b5b90ad3814e99509f3fe2cf6a1584bbe29b2
Jeff Grunewald <7341128+jeffgrunewald@users.noreply.github.com>
2025-07-16 17:09:43 -0400

    make set global config settings optional (#835)
    ## Summary of Changes
    * This change converts all values to the cli and sdk to be options,
    while still performing a validation that an initial complete config is
    enforced as well as rejecting an "empty" update
    * The global config consists of several values that should be capable of
    being updated independently of one another once the initial config is
    populated.
    
    ## Testing Verification
    * Updated unit tests in the cli command
    * Added e2e/sdk_serviceability_test module to the e2e suite to check the
    update from the integration level

--- smartcontract/sdk/rs/src/commands/globalconfig/set.rs
+++ smartcontract/sdk/rs/src/commands/globalconfig/set.rs
@@ -1,18 +1,19 @@
 use doublezero_serviceability::{
     instructions::DoubleZeroInstruction, pda::get_globalconfig_pda,
-    processors::globalconfig::set::SetGlobalConfigArgs, types::NetworkV4,
+    processors::globalconfig::set::SetGlobalConfigArgs, state::globalconfig::GlobalConfig,
+    types::NetworkV4,
 };
-use solana_sdk::{instruction::AccountMeta, signature::Signature};
+use solana_sdk::{instruction::AccountMeta, pubkey::Pubkey, signature::Signature};
 
-use crate::{DoubleZeroClient, GetGlobalStateCommand};
+use crate::{DoubleZeroClient, GetGlobalConfigCommand, GetGlobalStateCommand};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct SetGlobalConfigCommand {
-    pub local_asn: u32,
-    pub remote_asn: u32,
-    pub device_tunnel_block: NetworkV4,
-    pub user_tunnel_block: NetworkV4,
-    pub multicastgroup_block: NetworkV4,
+    pub local_asn: Option<u32>,
+    pub remote_asn: Option<u32>,
+    pub device_tunnel_block: Option<NetworkV4>,
+    pub user_tunnel_block: Option<NetworkV4>,
+    pub multicastgroup_block: Option<NetworkV4>,
 }
 
 impl SetGlobalConfigCommand {
@@ -21,19 +22,60 @@ impl SetGlobalConfigCommand {
             .execute(client)
             .map_err(|_err| eyre::eyre!("Globalstate not initialized"))?;
 
+        let global_config = GetGlobalConfigCommand.execute(client).ok();
+        let set_config_args = self.merge_config_updates(global_config)?;
+
         let (pda_pubkey, _) = get_globalconfig_pda(&client.get_program_id());
         client.execute_transaction(
-            DoubleZeroInstruction::SetGlobalConfig(SetGlobalConfigArgs {
-                local_asn: self.local_asn,
-                remote_asn: self.remote_asn,
-                device_tunnel_block: self.device_tunnel_block,
-                user_tunnel_block: self.user_tunnel_block,
-                multicastgroup_block: self.multicastgroup_block,
-            }),
+            DoubleZeroInstruction::SetGlobalConfig(set_config_args),
             vec![
                 AccountMeta::new(pda_pubkey, false),
                 AccountMeta::new(globalstate_pubkey, false),
             ],
         )
+    }
+
+    fn merge_config_updates(
+        &self,
+        global_config: Option<(Pubkey, GlobalConfig)>,
+    ) -> eyre::Result<SetGlobalConfigArgs> {
+        match (self, global_config) {
+            (
+                SetGlobalConfigCommand {
+                    local_asn: None,
+                    remote_asn: None,
+                    device_tunnel_block: None,
+                    user_tunnel_block: None,
+                    multicastgroup_block: None,
+                },
+                _,
+            ) => Err(eyre::eyre!(
+                "Invalid SetGlobalConfigCommand; no updates specified"
+            )),
+            (
+                SetGlobalConfigCommand {
+                    local_asn: Some(local_asn),
+                    remote_asn: Some(remote_asn),
+                    device_tunnel_block: Some(device_tunnel_block),
+                    user_tunnel_block: Some(user_tunnel_block),
+                    multicastgroup_block: Some(multicastgroup_block),
+                },
+                _,
+            ) => Ok(SetGlobalConfigArgs {
+                local_asn: *local_asn,
+                remote_asn: *remote_asn,
+                device_tunnel_block: *device_tunnel_block,
+                user_tunnel_block: *user_tunnel_block,
+                multicastgroup_block: *multicastgroup_block,
+            }),
+            (_, None) => Err(eyre::eyre!("Invalid SetGlobalConfigCommand; incomplete set command with no valid config to update")),
+            (set_config_command, Some((_, existing_config))) => Ok(SetGlobalConfigArgs {
+                local_asn: set_config_command.local_asn.unwrap_or(existing_config.local_asn),
+                remote_asn: set_config_command.remote_asn.unwrap_or(existing_config.remote_asn),
+                device_tunnel_block: set_config_command.device_tunnel_block.unwrap_or(existing_config.device_tunnel_block),
+                user_tunnel_block: set_config_command.user_tunnel_block.unwrap_or(existing_config.user_tunnel_block),
+                multicastgroup_block: set_config_command.multicastgroup_block.unwrap_or(existing_config.multicastgroup_block),
+            }),
+        }
     }
 }

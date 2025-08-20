93186bb8c13127ae66e05c7e833e49b95a85ee64
Juan Olveira <jolveira@gmail.com>
2025-08-20 15:32:21 +0000

    fix: update accesspass owner check to use payer field in user creation functions

--- smartcontract/programs/doublezero-serviceability/src/processors/user/create_subscribe.rs
+++ smartcontract/programs/doublezero-serviceability/src/processors/user/create_subscribe.rs
@@ -95,7 +95,7 @@ pub fn process_create_subscribe_user(
 
     // Read Access Pass
     let mut accesspass = AccessPass::try_from(accesspass_account)?;
-    if accesspass.owner != *payer_account.key || accesspass.client_ip != value.client_ip {
+    if accesspass.payer != *payer_account.key || accesspass.client_ip != value.client_ip {
         return Err(DoubleZeroError::Unauthorized.into());
     }
 

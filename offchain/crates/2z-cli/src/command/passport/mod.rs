ceafd21d43dab0cfb5b0f2bcb3f0b7f9ef0f41a5
Karl <214665535+karl-dz@users.noreply.github.com>
2025-08-25 14:17:19 -0500

    2z-cli: passport request Solana validator access (malbeclabs/doublezero-offchain#38)
    Closes https://github.com/malbeclabs/doublezero/issues/1355.

--- offchain/crates/2z-cli/src/command/passport/mod.rs
+++ offchain/crates/2z-cli/src/command/passport/mod.rs
@@ -1,9 +1,22 @@
-use anyhow::{Result, anyhow};
-use clap::{Args, Subcommand};
-use doublezero_passport::state::ProgramConfig;
-use doublezero_program_tools::zero_copy;
+use std::str::FromStr;
 
-use crate::rpc::{Connection, SolanaConnectionOptions};
+use anyhow::{Result, anyhow, bail};
+use clap::{Args, Subcommand};
+use doublezero_passport::{
+    ID,
+    instruction::{AccessMode, PassportInstructionData, account::RequestAccessAccounts},
+    state::{AccessRequest, ProgramConfig},
+};
+use doublezero_program_tools::{instruction::try_build_instruction, zero_copy};
+use solana_sdk::{
+    compute_budget::ComputeBudgetInstruction, offchain_message::OffchainMessage, pubkey::Pubkey,
+    signature::Signature,
+};
+
+use crate::{
+    payer::{SolanaPayerOptions, Wallet},
+    rpc::{Connection, SolanaConnectionOptions},
+};
 
 #[derive(Debug, Args)]
 pub struct PassportCliCommand {
@@ -20,6 +33,23 @@ pub enum PassportSubCommand {
         #[command(flatten)]
         solana_connection_options: SolanaConnectionOptions,
     },
+
+    RequestSolanaValidatorAccess {
+        service_key: Pubkey,
+
+        #[arg(long, value_name = "PUBKEY")]
+        node_id: Pubkey,
+
+        #[arg(long, short = 's', value_name = "BASE58_STRING")]
+        signature: String,
+
+        /// Offchain message version. ONLY 0 IS SUPPORTED.
+        #[arg(long, value_name = "U8", default_value = "0")]
+        message_version: u8,
+
+        #[command(flatten)]
+        solana_payer_options: SolanaPayerOptions,
+    },
 }
 
 impl PassportSubCommand {
@@ -29,6 +59,22 @@ impl PassportSubCommand {
                 program_config,
                 solana_connection_options,
             } => execute_fetch(program_config, solana_connection_options).await,
+            PassportSubCommand::RequestSolanaValidatorAccess {
+                service_key,
+                node_id,
+                signature,
+                message_version,
+                solana_payer_options,
+            } => {
+                execute_request_solana_validator_access(
+                    service_key,
+                    node_id,
+                    signature,
+                    message_version,
+                    solana_payer_options,
+                )
+                .await
+            }
         }
     }
 }
@@ -55,6 +101,76 @@ async fn execute_fetch(
 
         // TODO: Pretty print.
         println!("Program config: {program_config:?}");
+    }
+
+    Ok(())
+}
+
+//
+// PassportSubCommand::RequestSolanaValidatorAccess.
+//
+
+async fn execute_request_solana_validator_access(
+    service_key: Pubkey,
+    node_id: Pubkey,
+    signature: String,
+    message_version: u8,
+    solana_payer_options: SolanaPayerOptions,
+) -> Result<()> {
+    let verbose = solana_payer_options.signer_options.verbose;
+
+    let wallet = Wallet::try_from(solana_payer_options)?;
+    let wallet_key = wallet.pubkey();
+
+    let ed25519_signature = Signature::from_str(&signature)?;
+
+    // Verify the signature.
+    let raw_message = AccessRequest::access_request_message(&service_key);
+
+    if verbose {
+        println!("Raw message: {raw_message}");
+    }
+
+    let message = OffchainMessage::new(message_version, raw_message.as_bytes())?;
+    let serialized_message = message.serialize()?;
+
+    if !ed25519_signature.verify(node_id.as_array(), &serialized_message) {
+        bail!("Signature verification failed");
+    } else if verbose {
+        println!("Signature recovers node ID: {node_id}");
+    }
+
+    let request_access_ix = try_build_instruction(
+        &ID,
+        RequestAccessAccounts::new(&wallet_key, &service_key),
+        &PassportInstructionData::RequestAccess(AccessMode::SolanaValidator {
+            validator_id: node_id,
+            service_key,
+            ed25519_signature: ed25519_signature.into(),
+        }),
+    )?;
+
+    let mut compute_unit_limit = 10_000;
+
+    let (_, bump) = AccessRequest::find_address(&service_key);
+    compute_unit_limit += Wallet::compute_units_for_bump_seed(bump);
+
+    let mut instructions = vec![
+        request_access_ix,
+        ComputeBudgetInstruction::set_compute_unit_limit(compute_unit_limit),
+    ];
+
+    if let Some(ref compute_unit_price_ix) = wallet.compute_unit_price_ix {
+        instructions.push(compute_unit_price_ix.clone());
+    }
+
+    let transaction = wallet.new_transaction(&instructions).await?;
+    let tx_sig = wallet.send_or_simulate_transaction(&transaction).await?;
+
+    if let Some(tx_sig) = tx_sig {
+        println!("Request Solana validator access: {tx_sig}");
+
+        wallet.print_verbose_output(&[tx_sig]).await?;
     }
 
     Ok(())

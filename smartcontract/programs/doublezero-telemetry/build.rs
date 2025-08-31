99357af381203d18fb5d992ca397791843c78bca
Steven Normore <snormore@gmail.com>
2025-08-31 10:28:31 -0400

    sc/telemetry: use localnet from doublezero-config in build.rs

--- smartcontract/programs/doublezero-telemetry/build.rs
+++ smartcontract/programs/doublezero-telemetry/build.rs
@@ -1,9 +1,9 @@
-use std::{env, fs, path::Path};
+use std::path::Path;
 
 use doublezero_config::Environment;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = std::env::var("OUT_DIR").unwrap();
     let dest = Path::new(&out_dir).join("build_constants.rs");
 
     // Determine the network environment config with serviceability program ID based on the
@@ -11,34 +11,25 @@ fn main() {
     // the values being used at build-time.
     // The CARGO_FEATURE_* env variables are set by cargo for build scripts based on the features
     // defined in the Cargo.toml file.
-    let environment: Option<Environment> = if std::env::var("CARGO_FEATURE_MAINNET_BETA").is_ok() {
-        Some(Environment::MainnetBeta)
+    let env = if std::env::var("CARGO_FEATURE_MAINNET_BETA").is_ok() {
+        Environment::MainnetBeta
     } else if std::env::var("CARGO_FEATURE_TESTNET").is_ok() {
-        Some(Environment::Testnet)
+        Environment::Testnet
     } else if std::env::var("CARGO_FEATURE_DEVNET").is_ok() {
-        Some(Environment::Devnet)
+        Environment::Devnet
     } else {
-        None
+        Environment::Localnet
     };
 
-    let (env_code, serviceability_program_id) = match environment {
-        Some(environment) => (
-            environment.to_string(),
-            environment.config().serviceability_program_id.to_string(),
-        ),
-        None => (
-            "localnet".to_string(),
-            "7CTniUa88iJKUHTrCkB4TjAoG6TD7AMivhQeuqN2LPtX".to_string(),
-        ),
-    };
+    let serviceability_program_id = env.config().serviceability_program_id.to_string();
 
-    println!("cargo:warning=Environment: {}", env_code);
+    println!("cargo:warning=Environment: {}", env.moniker());
     println!(
         "cargo:warning=Serviceability Program ID: {}",
         serviceability_program_id
     );
 
-    fs::write(
+    std::fs::write(
         dest,
         format!(r#"pub const SERVICEABILITY_PROGRAM_ID: &str = "{serviceability_program_id}";"#),
     )

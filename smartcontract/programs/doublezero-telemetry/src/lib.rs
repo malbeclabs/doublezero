3ef2644120fb644e606eeb17aaecf0245006a0b8
Steven Normore <snormore@gmail.com>
2025-09-02 12:39:49 -0400

    Revert "sc/telemetry: embed serviceability program id via build feature" (#1502)
    Reverts malbeclabs/doublezero#1485 - devnet smartcontract deploy build
    is failing

--- smartcontract/programs/doublezero-telemetry/src/lib.rs
+++ smartcontract/programs/doublezero-telemetry/src/lib.rs
@@ -11,17 +11,35 @@ pub mod seeds;
 pub mod state;
 
 use solana_program::pubkey::Pubkey;
+use std::str::FromStr;
+
+const LOCAL_SERVICEABILITY_PROGRAM_ID: &str = "7CTniUa88iJKUHTrCkB4TjAoG6TD7AMivhQeuqN2LPtX";
+const DEVNET_SERVICEABILITY_PROGRAM_ID: &str = "GYhQDKuESrasNZGyhMJhGYFtbzNijYhcrN9poSqCQVah";
+const TESTNET_SERVICEABILITY_PROGRAM_ID: &str = "DZtnuQ839pSaDMFG5q1ad2V95G82S5EC4RrB3Ndw2Heb";
+const MAINNET_SERVICEABILITY_PROGRAM_ID: &str = "ser2VaTMAcYTaauMrTSfSrxBaUDq7BLNs2xfUugTAGv";
 
 #[cfg(not(test))]
 mod build_constants {
-    include!(concat!(env!("OUT_DIR"), "/build_constants.rs"));
+    include!(concat!(
+        env!("OUT_DIR"),
+        "/serviceability_program_id_input.rs"
+    ));
 }
 
-pub const fn serviceability_program_id() -> Pubkey {
-    Pubkey::from_str_const(crate::build_constants::SERVICEABILITY_PROGRAM_ID)
+pub fn serviceability_program_id() -> Pubkey {
+    let raw = match build_constants::RAW_SERVICEABILITY_PROGRAM_ID {
+        "local" => LOCAL_SERVICEABILITY_PROGRAM_ID,
+        "devnet" => DEVNET_SERVICEABILITY_PROGRAM_ID,
+        "testnet" => TESTNET_SERVICEABILITY_PROGRAM_ID,
+        "mainnet-beta" => MAINNET_SERVICEABILITY_PROGRAM_ID,
+        other => other,
+    };
+
+    let error_msg = format!("Invalid SERVICEABILITY_PROGRAM_ID=\"{raw}\"");
+    Pubkey::from_str(raw).expect(&error_msg)
 }
 
 #[cfg(test)]
 mod build_constants {
-    pub const SERVICEABILITY_PROGRAM_ID: &str = "";
+    pub const RAW_SERVICEABILITY_PROGRAM_ID: &str = "local";
 }

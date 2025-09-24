9e81a7fefa675d5070d26958eeb888504d021820
Juan Olveira <juan@malbeclabs.com>
2025-09-24 05:32:33 -0500

    Add contributor data to export functionality (#1716)
    This pull request adds contributor information to the device export
    functionality in the CLI. The main changes involve fetching contributor
    data, updating the device data structure to include contributor details,
    and ensuring contributor information is output during export.
    
    **Export enhancements:**
    
    * Added a `contributor` field to the `DeviceData` struct to store
    contributor information for each device.
    * Updated the export logic to fetch contributors using
    `ListContributorCommand` and retrieve contributor details for each
    device.
    [[1]](diffhunk://#diff-b1fc0e1fad86c28a689b0a58130bb31d6d02b6a7b60d3179613912a501ef6dc2R98)
    [[2]](diffhunk://#diff-b1fc0e1fad86c28a689b0a58130bb31d6d02b6a7b60d3179613912a501ef6dc2R113-R123)
    * Modified the export output to include the contributor code for each
    device.
    
    **Imports and dependencies:**
    
    * Added the `contributor` module to the list of imports in the file to
    support contributor-related commands.
    
    ## Testing Verification
    * Show evidence of testing the change
    
    ---------
    
    Co-authored-by: jolveira@gmail.com <jolveira@gmail.com>

--- smartcontract/cli/src/export.rs
+++ smartcontract/cli/src/export.rs
@@ -2,7 +2,7 @@ use crate::doublezerocommand::CliCommand;
 use clap::Args;
 use doublezero_program_common::types::parse_utils::bandwidth_to_string;
 use doublezero_sdk::commands::{
-    device::list::ListDeviceCommand, exchange::list::ListExchangeCommand,
+    contributor, device::list::ListDeviceCommand, exchange::list::ListExchangeCommand,
     link::list::ListLinkCommand, location::list::ListLocationCommand, user::list::ListUserCommand,
 };
 use serde::{Deserialize, Serialize};
@@ -24,6 +24,7 @@ struct Data {
 struct DeviceData {
     name: String,
     pubkey: String,
+    contributor: String,
     public_ip: String,
     location: LocationData,
     exchange: ExchangeData,
@@ -57,6 +58,7 @@ struct ExchangeData {
 struct LinkData {
     pub pubkey: String,
     pub code: String,
+    pub iface_name: String,
     pub side: LinkSideData,
     pub tunnel_net: String,
     pub link_type: String,
@@ -74,6 +76,7 @@ struct LinkSideData {
     pub tunnel_id: u16,
     pub tunnel_net: String,
     pub public_ip: String,
+    pub iface_name: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -94,6 +97,7 @@ impl ExportCliCommand {
         let locations = client.list_location(ListLocationCommand)?;
         let exchanges = client.list_exchange(ListExchangeCommand)?;
 
+        let contributors = client.list_contributor(contributor::list::ListContributorCommand {})?;
         let devices = client.list_device(ListDeviceCommand)?;
         let tunnels = client.list_link(ListLinkCommand)?;
         let users = client.list_user(ListUserCommand)?;
@@ -108,12 +112,17 @@ impl ExportCliCommand {
                 .get(&data.exchange_pk)
                 .ok_or(eyre::eyre!("Unable to retrieve Exchange"))?;
 
+            let contributor = contributors
+                .get(&data.contributor_pk)
+                .ok_or(eyre::eyre!("Unable to retrieve Contributor"))?;
+
             writeln!(out, "{name}")?;
 
             let config = Data {
                 device: DeviceData {
                     name: data.code,
                     pubkey: pubkey.to_string(),
+                    contributor: contributor.code.clone(),
                     location: LocationData {
                         code: location.code.clone(),
                         name: location.name.clone(),
@@ -139,20 +148,35 @@ impl ExportCliCommand {
                             tunnel.side_a_pk == pubkey || tunnel.side_z_pk == pubkey
                         })
                         .filter_map(|(key, link)| {
+                            let iface_name = if link.side_a_pk == pubkey {
+                                link.side_a_iface_name.clone()
+                            } else {
+                                link.side_z_iface_name.clone()
+                            };
+
                             let side_pubkey = if link.side_a_pk == pubkey {
                                 link.side_z_pk
                             } else {
                                 link.side_a_pk
                             };
+
+                            let iface_name2 = if link.side_a_pk == pubkey {
+                                link.side_z_iface_name.clone()
+                            } else {
+                                link.side_a_iface_name.clone()
+                            };
+
                             let side_device = devices.get(&side_pubkey)?;
 
                             Some(LinkData {
                                 pubkey: key.to_string(),
                                 code: link.code.clone(),
                                 tunnel_net: link.tunnel_net.to_string(),
+                                iface_name,
                                 side: LinkSideData {
                                     name: side_device.code.clone(),
                                     pubkey: side_pubkey.to_string(),
+                                    iface_name: iface_name2,
                                     public_ip: side_device.public_ip.to_string(),
                                     tunnel_id: link.tunnel_id,
                                     tunnel_net: link.tunnel_net.to_string(),

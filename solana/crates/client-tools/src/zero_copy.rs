978686cc4983b355a13bdc658ebd77955926d0a9
Karl <214665535+karl-dz@users.noreply.github.com>
2025-10-02 17:25:52 -0500

    client-tools: make zero-copy data optional and preserve other account info (malbeclabs/doublezero-solana#82)

--- solana/crates/client-tools/src/zero_copy.rs
+++ solana/crates/client-tools/src/zero_copy.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use bytemuck::Pod;
 use doublezero_program_tools::{zero_copy, PrecomputedDiscriminator};
 use solana_client::nonblocking::rpc_client::RpcClient;
@@ -6,8 +6,7 @@ use solana_sdk::pubkey::Pubkey;
 
 #[derive(Debug)]
 pub struct ZeroCopyAccountOwned<T: Pod + PrecomputedDiscriminator> {
-    pub data: T,
-    pub remaining_data: Vec<u8>,
+    pub data: Option<(Box<T>, Vec<u8>)>,
     pub lamports: u64,
     pub balance: u64,
     pub owner: Pubkey,
@@ -17,9 +16,8 @@ impl<T: Pod + PrecomputedDiscriminator> ZeroCopyAccountOwned<T> {
     pub async fn from_rpc_client(rpc_client: &RpcClient, account_key: &Pubkey) -> Result<Self> {
         let account_info = rpc_client.get_account(account_key).await?;
 
-        let (mucked_data, remaining_data) =
-            zero_copy::checked_from_bytes_with_discriminator(&account_info.data)
-                .ok_or(anyhow!("Cannot read as zero-copy account"))?;
+        let data = zero_copy::checked_from_bytes_with_discriminator(&account_info.data)
+            .map(|(mucked_data, remaining_data)| (Box::new(*mucked_data), remaining_data.to_vec()));
 
         let lamports = account_info.lamports;
 
@@ -29,8 +27,7 @@ impl<T: Pod + PrecomputedDiscriminator> ZeroCopyAccountOwned<T> {
         let balance = lamports.saturating_sub(rent_exemption);
 
         Ok(Self {
-            data: *mucked_data,
-            remaining_data: remaining_data.to_vec(),
+            data,
             lamports,
             balance,
             owner: account_info.owner,

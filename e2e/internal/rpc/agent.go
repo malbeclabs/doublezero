dc52b45579650326965c22bb07f4b6d78f2850e5
nikw9944 <nik@malbeclabs.com>
2025-10-30 09:55:30 -0400

    e2e: qa: wait for multicast tunnel to be up before starting connectivity test (#2014)
    ## Summary of Changes
    * Update qaagent ConnectMulticast to wait for the tunnel to come up
    before returning to avoid connectivity test failures
    * Add device code to logging output to assist with troubleshooting

--- e2e/internal/rpc/agent.go
+++ e2e/internal/rpc/agent.go
@@ -211,10 +211,13 @@ func (q *QAAgent) ConnectUnicast(ctx context.Context, req *pb.ConnectUnicastRequ
 
 	q.log.Info("Successfully connected IBRL mode tunnel")
 
+	current_device := "unknown"
+
 	condition := func() (bool, error) {
 		ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
 		defer cancel()
 		status, err := q.fetchStatus(ctx)
+		current_device = status[0].CurrentDevice
 		if err != nil {
 			q.log.Warn("fetchStatus error", "error", err)
 			return false, err
@@ -224,13 +227,14 @@ func (q *QAAgent) ConnectUnicast(ctx context.Context, req *pb.ConnectUnicastRequ
 			return false, fmt.Errorf("empty status response")
 		}
 		currentState := status[0].Response.DoubleZeroStatus.SessionStatus
-		q.log.Info("Polling tunnel status", "state", currentState, "tunnel_name", status[0].Response.TunnelName, "doublezero_ip", status[0].Response.DoubleZeroIP)
+		q.log.Info("Polling IBRL mode tunnel status", "state", currentState, "tunnel_name", status[0].Response.TunnelName, "doublezero_ip", status[0].Response.DoubleZeroIP, "current_device", current_device)
 		return currentState == "up", nil
 	}
 
 	err = poll.Until(ctx, condition, 60*time.Second, 1*time.Second)
 	if err != nil {
-		return nil, fmt.Errorf("failed while polling for session status: %v", err)
+		q.log.Error("Failed while polling for IBRL mode session status", "error", err)
+		return nil, fmt.Errorf("failed while polling for IBRL mode session status (device %s): %v", current_device, err)
 	}
 
 	return res, nil
@@ -344,7 +348,8 @@ func (q *QAAgent) DeleteMulticastGroup(ctx context.Context, req *pb.DeleteMultic
 }
 
 // ConnectMulticast implements the ConnectMulticast RPC, which connects to a multicast group
-// as either a publisher or subscriber.
+// as either a publisher or subscriber. This call will block until the tunnel is up according to
+// the DoubleZero status output or return an error if the tunnel is not up within 60 seconds.
 func (q *QAAgent) ConnectMulticast(ctx context.Context, req *pb.ConnectMulticastRequest) (*pb.Result, error) {
 	if req.GetCode() == "" {
 		return nil, fmt.Errorf("code is required")
@@ -366,6 +371,31 @@ func (q *QAAgent) ConnectMulticast(ctx context.Context, req *pb.ConnectMulticast
 		q.log.Error("Failed to connect multicast", "error", err, "output", result.Output)
 		return nil, err
 	}
+
+	q.log.Info("Successfully connected multicast tunnel")
+
+	condition := func() (bool, error) {
+		ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
+		defer cancel()
+		status, err := q.fetchStatus(ctx)
+		if err != nil {
+			q.log.Warn("fetchStatus error", "error", err)
+			return false, err
+		}
+		if len(status) == 0 {
+			q.log.Warn("fetchStatus returned empty status")
+			return false, fmt.Errorf("empty status response")
+		}
+		currentState := status[0].Response.DoubleZeroStatus.SessionStatus
+		q.log.Info("Polling multicast tunnel status", "state", currentState, "tunnel_name", status[0].Response.TunnelName, "doublezero_ip", status[0].Response.DoubleZeroIP)
+		return currentState == "up", nil
+	}
+
+	err = poll.Until(ctx, condition, 60*time.Second, 1*time.Second)
+	if err != nil {
+		return nil, fmt.Errorf("failed while polling for multicast session status: %v", err)
+	}
+
 	return result, nil
 }
 

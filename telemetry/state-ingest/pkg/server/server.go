012a532d21e92370b4dbc0bd75d0aa1b397befe8
Steven Normore <snormore@gmail.com>
2025-12-10 14:11:12 -0500

    telemetry: wip state ingest

--- telemetry/state-ingest/pkg/server/server.go
+++ telemetry/state-ingest/pkg/server/server.go
@@ -181,17 +181,14 @@ func (s *Server) uploadURLHandler(w http.ResponseWriter, r *http.Request) {
 	snapTS = snapTS.UTC()
 
 	date := snapTS.Format("2006-01-02")
-	hour := snapTS.Format("15")
-	filename := snapTS.Format("20060102T150405Z") + ".json"
+	filename := fmt.Sprintf("%s.json", req.SnapshotSHA256)
 
 	key := fmt.Sprintf(
-		"snapshots/%s/%s/%s/%s/%s/%s/%s",
+		"snapshots/kind=%s/device=%s/date=%s/timestamp=%s/%s",
 		req.Kind,
 		req.DevicePubkey,
 		date,
-		hour,
 		req.SnapshotTimestamp,
-		req.SnapshotSHA256,
 		filename,
 	)
 	if s.prefix != "" {

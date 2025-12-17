5eb7728c57b268143cd56a2ad6987d9e7882c8c7
jolveira@gmail.com <jolveira@gmail.com>
2025-12-17 16:12:46 +0000

    Update contributor, device, exchange, link, location, and multicast group commands to ignore case when matching codes
    Implement Default trait for Link, MulticastGroup, and update Get commands to support case-insensitive code matching

--- smartcontract/sdk/rs/src/commands/device/get.rs
+++ smartcontract/sdk/rs/src/commands/device/get.rs
@@ -20,7 +20,9 @@ impl GetDeviceCommand {
                 .gets(AccountType::Device)?
                 .into_iter()
                 .find(|(_, v)| match v {
-                    AccountData::Device(device) => device.code == self.pubkey_or_code,
+                    AccountData::Device(device) => {
+                        device.code.eq_ignore_ascii_case(&self.pubkey_or_code)
+                    }
                     _ => false,
                 })
                 .map(|(pk, v)| match v {
@@ -34,5 +36,99 @@ impl GetDeviceCommand {
                     ))
                 }),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{commands::device::get::GetDeviceCommand, tests::utils::create_test_client};
+    use doublezero_serviceability::state::{
+        accountdata::AccountData, accounttype::AccountType, device::Device,
+    };
+    use mockall::predicate;
+    use solana_sdk::pubkey::Pubkey;
+
+    #[test]
+    fn test_commands_device_get_command() {
+        let mut client = create_test_client();
+
+        let device_pubkey = Pubkey::new_unique();
+        let device = Device {
+            account_type: AccountType::Device,
+            index: 1,
+            bump_seed: 2,
+            reference_count: 0,
+            code: "device_code".to_string(),
+            owner: Pubkey::new_unique(),
+            ..Default::default()
+        };
+
+        let device2 = device.clone();
+        client
+            .expect_get()
+            .with(predicate::eq(device_pubkey))
+            .returning(move |_| Ok(AccountData::Device(device2.clone())));
+
+        let device2 = device.clone();
+        client
+            .expect_gets()
+            .with(predicate::eq(AccountType::Device))
+            .returning(move |_| {
+                Ok(HashMap::from([(
+                    device_pubkey,
+                    AccountData::Device(device2.clone()),
+                )]))
+            });
+
+        // Search by pubkey
+        let res = GetDeviceCommand {
+            pubkey_or_code: device_pubkey.to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "device_code".to_string());
+        assert_eq!(res.1.owner, device.owner);
+
+        // Search by code
+        let res = GetDeviceCommand {
+            pubkey_or_code: "device_code".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "device_code".to_string());
+        assert_eq!(res.1.owner, device.owner);
+
+        // Search by code UPPERCASE
+        let res = GetDeviceCommand {
+            pubkey_or_code: "DEVICE_CODE".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "device_code".to_string());
+        assert_eq!(res.1.owner, device.owner);
+
+        // Invalid search
+        let res = GetDeviceCommand {
+            pubkey_or_code: "ssssssssssss".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_err());
+
+        // Search by invalid code
+        let res = GetDeviceCommand {
+            pubkey_or_code: "s(%".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_err());
     }
 }
--- smartcontract/sdk/rs/src/commands/link/get.rs
+++ smartcontract/sdk/rs/src/commands/link/get.rs
@@ -20,7 +20,9 @@ impl GetLinkCommand {
                 .gets(AccountType::Link)?
                 .into_iter()
                 .find(|(_, v)| match v {
-                    AccountData::Link(tunnel) => tunnel.code == self.pubkey_or_code,
+                    AccountData::Link(tunnel) => {
+                        tunnel.code.eq_ignore_ascii_case(&self.pubkey_or_code)
+                    }
                     _ => false,
                 })
                 .map(|(pk, v)| match v {
@@ -34,5 +36,98 @@ impl GetLinkCommand {
                     ))
                 }),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{commands::link::get::GetLinkCommand, tests::utils::create_test_client};
+    use doublezero_serviceability::state::{
+        accountdata::AccountData, accounttype::AccountType, link::Link,
+    };
+    use mockall::predicate;
+    use solana_sdk::pubkey::Pubkey;
+
+    #[test]
+    fn test_commands_link_get_command() {
+        let mut client = create_test_client();
+
+        let link_pubkey = Pubkey::new_unique();
+        let link = Link {
+            account_type: AccountType::Link,
+            index: 1,
+            bump_seed: 2,
+            code: "link_code".to_string(),
+            owner: Pubkey::new_unique(),
+            ..Default::default()
+        };
+
+        let link2 = link.clone();
+        client
+            .expect_get()
+            .with(predicate::eq(link_pubkey))
+            .returning(move |_| Ok(AccountData::Link(link2.clone())));
+
+        let link2 = link.clone();
+        client
+            .expect_gets()
+            .with(predicate::eq(AccountType::Link))
+            .returning(move |_| {
+                Ok(HashMap::from([(
+                    link_pubkey,
+                    AccountData::Link(link2.clone()),
+                )]))
+            });
+
+        // Search by pubkey
+        let res = GetLinkCommand {
+            pubkey_or_code: link_pubkey.to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "link_code".to_string());
+        assert_eq!(res.1.owner, link.owner);
+
+        // Search by code
+        let res = GetLinkCommand {
+            pubkey_or_code: "link_code".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "link_code".to_string());
+        assert_eq!(res.1.owner, link.owner);
+
+        // Search by code UPPERCASE
+        let res = GetLinkCommand {
+            pubkey_or_code: "LINK_CODE".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert_eq!(res.1.code, "link_code".to_string());
+        assert_eq!(res.1.owner, link.owner);
+
+        // Invalid search
+        let res = GetLinkCommand {
+            pubkey_or_code: "ssssssssssss".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_err());
+
+        // Search by invalid code
+        let res = GetLinkCommand {
+            pubkey_or_code: "s(%".to_string(),
+        }
+        .execute(&client);
+
+        assert!(res.is_err());
     }
 }

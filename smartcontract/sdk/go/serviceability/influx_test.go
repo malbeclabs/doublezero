48e453dfc8348a30a0d102b53867b1ebbda8f3a9
Juan Olveira <juan@malbeclabs.com>
2026-01-12 16:06:10 +0000

    Update GO SDK
    fix String()
    
    Refactor DeviceDesiredStatus String method for clarity and error handling
    
    Fix LinkHealth String method to use underscores in status string
    
    fix: update expected output to include link_desired_status in test cases

--- smartcontract/sdk/go/serviceability/influx_test.go
+++ smartcontract/sdk/go/serviceability/influx_test.go
@@ -154,7 +154,7 @@ func TestToLineProtocol(t *testing.T) {
 				"env": "testnet",
 			},
 			expected: `links,code=link-01,contributor_pubkey=` + pubKey1B58 +
-				`,env=testnet,link_type=WAN,owner=` + pubKey1B58 +
+				`,env=testnet,link_desired_status=pending,link_type=WAN,owner=` + pubKey1B58 +
 				`,pubkey=` + pubKey1B58 +
 				`,side_a_iface_name=xe-0/0/0,side_a_pubkey=` + pubKey1B58 +
 				`,side_z_iface_name=xe-0/0/1,side_z_pubkey=` + pubKey1B58 +
@@ -185,7 +185,7 @@ func TestToLineProtocol(t *testing.T) {
 				"env": "testnet",
 			},
 			expected: `links,code=link-empty,contributor_pubkey=` + pubKey1B58 +
-				`,env=testnet,link_type=DZX,owner=` + pubKey1B58 +
+				`,env=testnet,link_desired_status=pending,link_type=DZX,owner=` + pubKey1B58 +
 				`,pubkey=` + pubKey1B58 +
 				`,side_a_pubkey=` + pubKey1B58 +
 				`,side_z_pubkey=` + pubKey1B58 +

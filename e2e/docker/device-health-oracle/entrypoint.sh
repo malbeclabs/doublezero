3fe70c21c93cc8d9252f8cfc1a1739ab4b073b30
Nik Weidenbacher <nik@malbeclabs.com>
2026-01-20 15:57:44 +0000

    add comment

--- e2e/docker/device-health-oracle/entrypoint.sh
+++ e2e/docker/device-health-oracle/entrypoint.sh
@@ -15,4 +15,5 @@ if [ -z "${DZ_TELEMETRY_PROGRAM_ID}" ]; then
   exit 1
 fi
 
+# start device-health-oracle
 device-health-oracle -ledger-rpc-url ${DZ_LEDGER_URL} -serviceability-program-id ${DZ_SERVICEABILITY_PROGRAM_ID} -telemetry-program-id ${DZ_TELEMETRY_PROGRAM_ID} -metrics-addr 0.0.0.0:8080 -interval 1m

a39354b9ef52cceff273feb6d4a56385a0fe3113
Steven Normore <snormore@gmail.com>
2026-01-21 14:19:14 -0500

    api: add carto map tiles to CSP

--- lake/api/main.go
+++ lake/api/main.go
@@ -163,10 +163,10 @@ func main() {
 				"default-src 'self'",
 				"script-src 'self' 'unsafe-inline' https://accounts.google.com https://static.cloudflareinsights.com",
 				"frame-src https://accounts.google.com https://accounts.googleusercontent.com",
-				"connect-src 'self' https://accounts.google.com https://cloudflareinsights.com",
+				"connect-src 'self' https://accounts.google.com https://cloudflareinsights.com https://*.basemaps.cartocdn.com",
 				"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com",
 				"font-src 'self' https://fonts.gstatic.com",
-				"img-src 'self' data: https://lh3.googleusercontent.com",
+				"img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.basemaps.cartocdn.com",
 			}, "; ")
 			w.Header().Set("Content-Security-Policy", csp)
 

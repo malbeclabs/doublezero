51c431ec2339018c1beddd46e6efe6db1d84cf52
Steven Normore <snormore@gmail.com>
2026-01-28 00:16:09 -0500

    lake/indexer: separate repo

--- lake/indexer/db/neo4j/migrations/000002_isis_schema.up.cypher
+++
@@ -1,1 +0,0 @@
-CREATE INDEX link_isis_metric_idx IF NOT EXISTS FOR (n:Link) ON (n.isis_metric);
--- lake/indexer/embed.go
+++
@@ -1,9 +0,0 @@
-package indexer
-
-import "embed"
-
-//go:embed db/clickhouse/migrations/*.sql
-var ClickHouseMigrationsFS embed.FS
-
-//go:embed db/neo4j/migrations/*.cypher
-var Neo4jMigrationsFS embed.FS

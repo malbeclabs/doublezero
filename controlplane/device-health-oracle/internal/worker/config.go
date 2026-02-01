8195075e61365f4084020fbbed0bfa6c368b9456
Steven Normore <snormore@gmail.com>
2026-02-01 11:39:52 -0500

    controlplane/funder,device-health-oracle: migrate to new serviceability SDK

--- controlplane/device-health-oracle/internal/worker/config.go
+++ controlplane/device-health-oracle/internal/worker/config.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/gagliardetto/solana-go"
 	solanarpc "github.com/gagliardetto/solana-go/rpc"
-	"github.com/malbeclabs/doublezero/smartcontract/sdk/go/serviceability"
+	serviceability "github.com/malbeclabs/doublezero/sdk/serviceability/go"
 	"github.com/malbeclabs/doublezero/smartcontract/sdk/go/telemetry"
 )
 
--- sdk/serviceability/go/client.go
+++ sdk/serviceability/go/client.go
@@ -28,6 +28,11 @@ type ProgramData struct {
 	AccessPasses    []AccessPass
 }
 
+// ProgramDataProvider is implemented by any type that can fetch program data.
+type ProgramDataProvider interface {
+	GetProgramData(ctx context.Context) (*ProgramData, error)
+}
+
 // Client provides read-only access to serviceability program accounts.
 type Client struct {
 	rpc       RPCClient

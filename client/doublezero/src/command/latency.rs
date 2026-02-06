d28477d393af333e833ae6296b91a321d29be7bc
Ben Marx <bgm@malbeclabs.com>
2026-02-05 18:06:42 -0800

    client/doublezero: revert VersionStatus JSON wrapper, add JSON format tests

--- client/doublezero/src/command/latency.rs
+++ client/doublezero/src/command/latency.rs
@@ -1,16 +1,11 @@
 use crate::command::util;
 use clap::Args;
-use doublezero_cli::{
-    checkversion::{get_version_status, VersionStatus},
-    doublezerocommand::CliCommand,
-};
-use doublezero_sdk::{commands::device::list::ListDeviceCommand, ProgramVersion};
-use serde::{Deserialize, Serialize};
+use doublezero_cli::doublezerocommand::CliCommand;
+use doublezero_sdk::commands::device::list::ListDeviceCommand;
 
 use crate::{
-    dzd_latency::retrieve_latencies,
-    requirements::check_doublezero,
-    servicecontroller::{LatencyRecord, ServiceControllerImpl},
+    dzd_latency::retrieve_latencies, requirements::check_doublezero,
+    servicecontroller::ServiceControllerImpl,
 };
 
 #[derive(Args, Debug)]
@@ -20,39 +15,15 @@ pub struct LatencyCliCommand {
     json: bool,
 }
 
-/// JSON response wrapper that includes version status information
-#[derive(Debug, Serialize, Deserialize)]
-struct LatencyJsonResponse {
-    version: VersionStatus,
-    latencies: Vec<LatencyRecord>,
-}
-
 impl LatencyCliCommand {
     pub async fn execute(self, client: &dyn CliCommand) -> eyre::Result<()> {
         let controller = ServiceControllerImpl::new(None);
         check_doublezero(&controller, client, None).await?;
 
-        // Get version status for JSON output
-        let version_status = get_version_status(client, ProgramVersion::current());
-
         let devices = client.list_device(ListDeviceCommand)?;
         let latencies = retrieve_latencies(&controller, &devices, false, None).await?;
 
-        if self.json {
-            // For JSON output, include version status in the response
-            let json_response = LatencyJsonResponse {
-                version: version_status,
-                latencies,
-            };
-            let output = serde_json::to_string_pretty(&json_response)?;
-            println!("{output}");
-        } else {
-            // For table output, print version warning to stderr if needed
-            if let Some(msg) = version_status.message() {
-                eprintln!("{msg}");
-            }
-            util::show_output(latencies, false)?;
-        }
+        util::show_output(latencies, self.json)?;
 
         Ok(())
     }

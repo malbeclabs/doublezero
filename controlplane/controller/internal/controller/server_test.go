c871de510b642be0c71313ea9b080eb61acb4780
Steven Normore <snormore@gmail.com>
2026-02-10 08:16:23 -0600

    sdk/serviceability: add missing Tenant account type to Go SDK

--- controlplane/controller/internal/controller/server_test.go
+++ controlplane/controller/internal/controller/server_test.go
@@ -1338,7 +1338,7 @@ func TestStateCache(t *testing.T) {
 		},
 		{
 			Name: "tenant_vrf_assignment",
-			Config: serviceability.Config{
+			GlobalConfig: serviceability.GlobalConfig{
 				MulticastGroupBlock: [5]uint8{239, 0, 0, 0, 24},
 			},
 			Exchanges: []serviceability.Exchange{
@@ -1428,7 +1428,7 @@ func TestStateCache(t *testing.T) {
 				},
 			},
 			StateCache: stateCache{
-				Config: serviceability.Config{
+				GlobalConfig: serviceability.GlobalConfig{
 					MulticastGroupBlock: [5]uint8{239, 0, 0, 0, 24},
 				},
 				MulticastGroups: map[string]serviceability.MulticastGroup{},
@@ -1937,7 +1937,7 @@ func TestEndToEnd(t *testing.T) {
 		},
 		{
 			Name: "tenant_vrf_end_to_end",
-			Config: serviceability.Config{
+			GlobalConfig: serviceability.GlobalConfig{
 				MulticastGroupBlock: [5]uint8{239, 0, 0, 0, 24},
 			},
 			MulticastGroups: []serviceability.MulticastGroup{

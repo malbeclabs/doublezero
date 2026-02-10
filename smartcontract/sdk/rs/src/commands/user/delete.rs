f619c0d3c7187021324ebde0a14f6729559807f2
Steven Normore <snormore@gmail.com>
2026-02-10 11:09:15 -0600

    sdk: add missing tenant_allowlist field in user delete test

--- smartcontract/sdk/rs/src/commands/user/delete.rs
+++ smartcontract/sdk/rs/src/commands/user/delete.rs
@@ -415,6 +415,7 @@ mod tests {
             mgroup_sub_allowlist: vec![mgroup_pubkey],
             tenant_allowlist: vec![],
             flags: 0,
+            tenant_allowlist: vec![],
         };
 
         let mut seq = Sequence::new();

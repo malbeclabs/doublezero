1c31228e1bf779613ca1631fde19509b9a909f22
Martin Sander <martin@malbeclabs.com>
2026-02-13 16:38:52 -0600

    serviceability: require Activated status for UnlinkDeviceInterface

--- activator/src/process/device.rs
+++ activator/src/process/device.rs
@@ -257,6 +257,12 @@ mod tests {
             assert!(devices.contains_key(&device_pubkey));
             assert_eq!(devices.get(&device_pubkey).unwrap().device, device);
 
+            // UnlinkDeviceInterfaceCommand now looks up links to discover associated accounts
+            client
+                .expect_gets()
+                .with(predicate::eq(AccountType::Link))
+                .returning(|_| Ok(HashMap::new()));
+
             client
                 .expect_execute_transaction()
                 .times(1)
@@ -472,6 +478,12 @@ mod tests {
         };
 
         let mut ip_block_allocator = IPBlockAllocator::new("1.1.1.0/24".parse().unwrap());
+
+        // UnlinkDeviceInterfaceCommand now looks up links to discover associated accounts
+        client
+            .expect_gets()
+            .with(predicate::eq(AccountType::Link))
+            .returning(|_| Ok(HashMap::new()));
 
         client
             .expect_execute_transaction()

a81ca0bc400f00cd984bbda099ff2bfeb6ed7d35
Martin Sander <martin@malbeclabs.com>
2026-02-20 15:24:54 -0600

    e2e: limit clickhouse publishing to devnet for initial rollout

--- e2e/qa_alldevices_unicast_test.go
+++ e2e/qa_alldevices_unicast_test.go
@@ -216,8 +216,10 @@ func TestQA_AllDevices_UnicastConnectivity(t *testing.T) {
 	if err := qa.PublishMetrics(ctx, log, qa.MetricsConfigFromEnv(), envArg, results, time.Since(startTime)); err != nil {
 		log.Error("Failed to publish metrics", "error", err)
 	}
-	if err := qa.PublishToClickhouse(ctx, log, qa.ClickhouseConfigFromEnv(), envArg, results, time.Since(startTime)); err != nil {
-		log.Error("Failed to publish metrics to ClickHouse", "error", err)
+	if envArg == "devnet" {
+		if err := qa.PublishToClickhouse(ctx, log, qa.ClickhouseConfigFromEnv(), envArg, results, time.Since(startTime)); err != nil {
+			log.Error("Failed to publish metrics to ClickHouse", "error", err)
+		}
 	}
 }
 

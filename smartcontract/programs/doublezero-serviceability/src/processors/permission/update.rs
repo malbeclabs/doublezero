570eb9b2dc101f14191224e63a9a8ecd273309a0
Juan Olveira <juan@malbeclabs.com>
2026-03-09 17:43:26 +0000

    smartcontract: implement Permission account CRUD and authorize() mechanism

--- smartcontract/programs/doublezero-serviceability/src/processors/permission/update.rs
+++ smartcontract/programs/doublezero-serviceability/src/processors/permission/update.rs
@@ -1,8 +1,20 @@
+use crate::{
+    authorize::authorize,
+    error::DoubleZeroError,
+    pda::get_permission_pda,
+    serializer::try_acc_write,
+    state::{
+        globalstate::GlobalState,
+        permission::{permission_flags, Permission},
+    },
+};
 use borsh::BorshSerialize;
 use borsh_incremental::BorshDeserializeIncremental;
 use core::fmt;
 use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
 
@@ -19,9 +31,53 @@ impl fmt::Debug for PermissionUpdateArgs {
 }
 
 pub fn process_update_permission(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _value: &PermissionUpdateArgs,
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    value: &PermissionUpdateArgs,
 ) -> ProgramResult {
-    Err(ProgramError::InvalidInstructionData)
+    let accounts_iter = &mut accounts.iter();
+
+    let permission_account = next_account_info(accounts_iter)?;
+    let globalstate_account = next_account_info(accounts_iter)?;
+    let payer_account = next_account_info(accounts_iter)?;
+    let _system_program = next_account_info(accounts_iter)?;
+
+    assert!(payer_account.is_signer, "Payer must be a signer");
+    assert_eq!(
+        globalstate_account.owner, program_id,
+        "Invalid GlobalState Account Owner"
+    );
+    assert_eq!(
+        permission_account.owner, program_id,
+        "Invalid Permission Account Owner"
+    );
+    assert!(
+        permission_account.is_writable,
+        "Permission Account is not writable"
+    );
+
+    let mut permission = Permission::try_from(permission_account)?;
+
+    let (expected_pda, _) = get_permission_pda(program_id, &permission.user_payer);
+    if permission_account.key != &expected_pda {
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    if value.permissions == 0 {
+        return Err(DoubleZeroError::InvalidArgument.into());
+    }
+
+    let globalstate = GlobalState::try_from(globalstate_account)?;
+    authorize(
+        program_id,
+        accounts_iter,
+        payer_account.key,
+        &globalstate,
+        permission_flags::PERMISSION_ADMIN,
+    )?;
+
+    permission.permissions = value.permissions;
+    try_acc_write(&permission, permission_account, payer_account, accounts)?;
+
+    Ok(())
 }

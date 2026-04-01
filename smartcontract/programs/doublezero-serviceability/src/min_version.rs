0bb1a83104af5b952a2855a0d60a4064b4155685
Greg Mitchell <greg@malbeclabs.com>
2026-04-01 13:37:00 -0400

    Bump minimum client version to 0.10.0 (#3429)

--- smartcontract/programs/doublezero-serviceability/src/min_version.rs
+++ smartcontract/programs/doublezero-serviceability/src/min_version.rs
@@ -1,2 +1,1 @@
-// Implementing Interfaces V2. Clients must have version 0.7.1 or higher to read V2 interfaces.
-pub const MIN_COMPATIBLE_VERSION: &str = "0.7.1";
+pub const MIN_COMPATIBLE_VERSION: &str = "0.10.0";

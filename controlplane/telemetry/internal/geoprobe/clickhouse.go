4ea60ead13f6d5f180a41ed329930806becaed8c
Nik Weidenbacher <nik@malbeclabs.com>
2026-04-03 20:53:54 +0000

    geoprobe: add 5s timeout to shutdown flush to avoid hanging if ClickHouse is unreachable

--- controlplane/telemetry/internal/geoprobe/clickhouse.go
+++ controlplane/telemetry/internal/geoprobe/clickhouse.go
@@ -149,7 +149,9 @@ func (w *ClickhouseWriter) Run(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			w.flush(context.Background())
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			w.flush(shutdownCtx)
+			cancel()
 			return
 		case <-ticker.C:
 			w.flush(ctx)

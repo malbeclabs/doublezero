f8ffb7dfb3dbf7a823924be822f1c1757dbd8039
Ben Cairns <ben@malbeclabs.com>
2026-04-07 13:45:25 -0500

    smartcontract: fix missing include_topologies field in cli user get test

--- smartcontract/cli/src/user/get.rs
+++ smartcontract/cli/src/user/get.rs
@@ -373,6 +373,7 @@ mod tests {
             metro_routing: false,
             route_liveness: false,
             billing: TenantBillingConfig::default(),
+            include_topologies: vec![],
         };
 
         let device_pubkey = Pubkey::new_unique();

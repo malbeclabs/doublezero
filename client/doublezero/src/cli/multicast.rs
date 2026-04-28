7078069fb1b3caf33a4e035cc658fc6f25137774
bgm-malbeclabs <bgm@malbeclabs.com>
2026-04-28 11:29:23 -0700

    feat(cli): allow unsubscribe without disconnect (#3577)
    ## Summary of Changes
    This PR adds the ability for a user to unsubscribe or no longer publish
    without the need for a disconnect. This is only for connections managed
    by the user, ie not the oracle. There's a warning if a user tries to
    unpublish or unsubscribe from their only remaining group.
    
    Closes https://github.com/malbeclabs/doublezero/issues/3575
    
    ## Testing Verification
    * Updated e2e test
    * Resolved a flaky single socket issue
    * rust rests

--- client/doublezero/src/cli/multicast.rs
+++ client/doublezero/src/cli/multicast.rs
@@ -13,4 +13,44 @@ pub enum MulticastCommands {
     /// Manage multicast groups
     #[clap()]
     Group(MulticastGroupCliCommand),
+    /// Subscribe to one or more multicast groups (user must already be connected)
+    #[clap()]
+    Subscribe(MulticastSubscribeCliCommand),
+    /// Unsubscribe from one or more multicast groups
+    #[clap()]
+    Unsubscribe(MulticastUnsubscribeCliCommand),
+    /// Publish to one or more multicast groups (user must already be connected)
+    #[clap()]
+    Publish(MulticastPublishCliCommand),
+    /// Stop publishing to one or more multicast groups
+    #[clap()]
+    Unpublish(MulticastUnpublishCliCommand),
+}
+
+#[derive(Args, Debug)]
+pub struct MulticastSubscribeCliCommand {
+    /// Multicast group code(s) to subscribe to
+    #[arg(num_args = 1..)]
+    pub groups: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct MulticastUnsubscribeCliCommand {
+    /// Multicast group code(s) to unsubscribe from
+    #[arg(num_args = 1..)]
+    pub groups: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct MulticastPublishCliCommand {
+    /// Multicast group code(s) to publish to
+    #[arg(num_args = 1..)]
+    pub groups: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct MulticastUnpublishCliCommand {
+    /// Multicast group code(s) to stop publishing to
+    #[arg(num_args = 1..)]
+    pub groups: Vec<String>,
 }

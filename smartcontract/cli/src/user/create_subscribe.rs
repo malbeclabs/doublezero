df5507970f4804d31bd19ef68c6eaed2229fc14e
Martin Sander <martin@malbeclabs.com>
2026-07-27 16:44:55 -0500

    cli: test user create-subscribe --feed code-resolution branch
    Companion to the pubkey test: a bare `--feed` code resolves through get_feed
    (exchange None) and the returned pubkey is passed as the trailing Feed account.

--- smartcontract/cli/src/user/create_subscribe.rs
+++ smartcontract/cli/src/user/create_subscribe.rs
@@ -156,10 +156,11 @@ mod tests {
     };
     use doublezero_sdk::{
         commands::{
-            device::get::GetDeviceCommand, multicastgroup::get::GetMulticastGroupCommand,
+            device::get::GetDeviceCommand, feed::get::GetFeedCommand,
+            multicastgroup::get::GetMulticastGroupCommand,
             user::create_subscribe::CreateSubscribeUserCommand,
         },
-        AccountType, Device, DeviceStatus, DeviceType, MulticastGroup, MulticastGroupStatus,
+        AccountType, Device, DeviceStatus, DeviceType, Feed, MulticastGroup, MulticastGroupStatus,
         UserCYOA, UserType,
     };
     use doublezero_serviceability::pda::get_user_old_pda;
@@ -399,5 +400,141 @@ mod tests {
             .execute(&ctx, &client, &mut output),
         );
         assert!(res.is_ok());
+    }
+
+    // A bare `--feed` code (not a pubkey) is resolved through `get_feed` with exchange
+    // None, and the resolved pubkey is passed as the trailing Feed account.
+    #[test]
+    fn test_cli_user_create_subscribe_with_feed_code() {
+        let mut client = create_test_client();
+
+        let (pda_pubkey, _bump_seed) = get_user_old_pda(&client.get_program_id(), 1);
+        let signature = Signature::from([
+            120, 138, 162, 185, 59, 209, 241, 157, 71, 157, 74, 131, 4, 87, 54, 28, 38, 180, 222,
+            82, 64, 62, 61, 62, 22, 46, 17, 203, 187, 136, 62, 43, 11, 38, 235, 17, 239, 82, 240,
+            139, 130, 217, 227, 214, 9, 242, 141, 223, 94, 29, 184, 110, 62, 32, 87, 137, 63, 139,
+            100, 221, 20, 137, 4, 5,
+        ]);
+
+        let device_pubkey = Pubkey::new_unique();
+        let device = Device {
+            account_type: AccountType::Device,
+            index: 1,
+            bump_seed: 255,
+            reference_count: 0,
+            code: "device1".to_string(),
+            contributor_pk: Pubkey::default(),
+            location_pk: Pubkey::default(),
+            exchange_pk: Pubkey::default(),
+            device_type: DeviceType::Hybrid,
+            public_ip: [10, 0, 0, 1].into(),
+            dz_prefixes: "10.0.0.1/24".parse().unwrap(),
+            owner: device_pubkey,
+            metrics_publisher_pk: Pubkey::default(),
+            status: DeviceStatus::Activated,
+            mgmt_vrf: "default".to_string(),
+            interfaces: vec![],
+            max_users: 255,
+            users_count: 0,
+            device_health: doublezero_serviceability::state::device::DeviceHealth::ReadyForUsers,
+            desired_status:
+                doublezero_serviceability::state::device::DeviceDesiredStatus::Activated,
+            unicast_users_count: 0,
+            multicast_subscribers_count: 0,
+            max_unicast_users: 0,
+            max_multicast_subscribers: 0,
+            reserved_seats: 0,
+            multicast_publishers_count: 0,
+            max_multicast_publishers: 0,
+            ..Default::default()
+        };
+        client
+            .expect_check_requirements()
+            .with(predicate::eq(CHECK_ID_JSON | CHECK_BALANCE))
+            .returning(|_| Ok(()));
+        client
+            .expect_get_device()
+            .with(predicate::eq(GetDeviceCommand {
+                pubkey_or_code: "device1".to_string(),
+            }))
+            .times(1)
+            .returning(move |_| Ok((device_pubkey, device.clone())));
+
+        let mgroup_pubkey = Pubkey::new_unique();
+        let mgroup = MulticastGroup {
+            account_type: AccountType::MulticastGroup,
+            owner: Pubkey::default(),
+            index: 1,
+            bump_seed: 1,
+            tenant_pk: Pubkey::default(),
+            code: "mg1".to_string(),
+            multicast_ip: [239, 0, 0, 1].into(),
+            max_bandwidth: 1_000_000_000,
+            status: MulticastGroupStatus::Activated,
+            publisher_count: 0,
+            subscriber_count: 0,
+        };
+        client
+            .expect_get_multicastgroup()
+            .with(predicate::eq(GetMulticastGroupCommand {
+                pubkey_or_code: mgroup_pubkey.to_string(),
+            }))
+            .times(1)
+            .returning(move |_| Ok((mgroup_pubkey, mgroup.clone())));
+
+        // "shreds-nyc" is not a pubkey, so parse_pubkey returns None and the CLI resolves
+        // it through get_feed (exchange None), then passes the returned pubkey through.
+        let resolved_feed_pubkey = Pubkey::new_unique();
+        let feed = Feed {
+            account_type: AccountType::Feed,
+            owner: Pubkey::new_unique(),
+            bump_seed: 255,
+            code: "shreds-nyc".to_string(),
+            name: "Shreds NYC".to_string(),
+            exchange: Pubkey::new_unique(),
+            groups: vec![],
+        };
+        client
+            .expect_get_feed()
+            .with(predicate::eq(GetFeedCommand {
+                pubkey_or_code: "shreds-nyc".to_string(),
+                exchange: None,
+            }))
+            .times(1)
+            .returning(move |_| Ok((resolved_feed_pubkey, feed.clone())));
+
+        client
+            .expect_create_subscribe_user()
+            .with(predicate::eq(CreateSubscribeUserCommand {
+                user_type: UserType::Multicast,
+                device_pk: device_pubkey,
+                cyoa_type: UserCYOA::GREOverDIA,
+                client_ip: [100, 0, 0, 1].into(),
+                publisher: false,
+                subscriber: true,
+                mgroup_pk: mgroup_pubkey,
+                tunnel_endpoint: Ipv4Addr::UNSPECIFIED,
+                owner: None,
+                feed_pk: Some(resolved_feed_pubkey),
+            }))
+            .times(1)
+            .returning(move |_| Ok((signature, pda_pubkey)));
+
+        let mut output = Vec::new();
+        let ctx = cli_context_default_for_tests();
+        let res = block_on(
+            CreateSubscribeUserCliCommand {
+                device: "device1".to_string(),
+                client_ip: [100, 0, 0, 1].into(),
+                allocate_addr: false,
+                publisher: None,
+                subscriber: Some(mgroup_pubkey.to_string()),
+                wait: false,
+                owner: None,
+                feed: Some("shreds-nyc".to_string()),
+            }
+            .execute(&ctx, &client, &mut output),
+        );
+        assert!(res.is_ok(), "{res:?}");
     }
 }
